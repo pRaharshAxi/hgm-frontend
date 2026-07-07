ebbdb46d97915bdcb2f65d1c9faa82dfda2b8435
Chamod Devranga <cdevranga@gmail.com>
2026-07-07 16:08:22 +0530

    h

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+<QueryClientProvider client={queryClient}>
+  <App/>
+</QueryClientProvider>;
